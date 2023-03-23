1bafeba52dac91c09790e8ad88bbcc7a7780887a
jaimemin <jaimemin@gmail.com>
2023-03-23 21:22:51 +0900

    liquibase changeSet sequence sql 수정

--- src/main/java/com/tistory/jaimemin/multidatasourcejpa/tenant/repository/ProductRepository.java
+++ src/main/java/com/tistory/jaimemin/multidatasourcejpa/tenant/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.tistory.jaimemin.multidatasourcejpa.tenant.repository;
 import com.tistory.jaimemin.multidatasourcejpa.tenant.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
